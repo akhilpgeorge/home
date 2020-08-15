0c633665536f64a8e78b923a1b59417d7ab88e58
akhilpgeorge <akhilpgeorge@gmail.com>
2020-08-15 13:51:15 +0530

    updates dp

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -41,7 +41,7 @@ const mainBody = {
 //b) a link to an hosted image
 //      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
 //c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/akhil-dp.jpeg"),
+//      i.e: profilePictureLink: require("../editable-stuff/about-me-dp.jpg"),
 //d) If you do not want any picture to be displayed, just leave it empty :)
 //      i.e: profilePictureLink: "",
 // For Resume either provide link to your resume or import from "editable-stuff" directory
@@ -51,7 +51,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/akhil-dp.jpeg"),
+  imageLink: require("../editable-stuff/about-me-dp.jpg"),
   imageSize: 375,
   message:
     "A software engineer with 2.5+ years experience of facilitating cutting-edge engineering solutions with a wide range of technology skills. Highly skilled in Front end technologies. enjoys research and with pivotal contributions in building innovative applications to the needs of the business",
@@ -78,8 +78,8 @@ const leadership = {
   message:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
-    { img: require("../editable-stuff/akhil-dp.jpeg") },
-    { img: require("../editable-stuff/akhil-dp.jpeg") },
+    { img: require("../editable-stuff/about-me-dp.jpg") },
+    { img: require("../editable-stuff/about-me-dp.jpg") },
   ],
 };
 
