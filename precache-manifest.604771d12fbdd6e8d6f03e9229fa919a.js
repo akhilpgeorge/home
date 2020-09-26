786bd52375287992670192a6d796a4caa93282ed
akhilpgeorge <akhilpgeorge@gmail.com>
2020-09-26 12:47:22 +0530

    Updates

---
+++ precache-manifest.604771d12fbdd6e8d6f03e9229fa919a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b1855b064362f8e44253c0802b2c0b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ea414c97ea58d3bc14cd",
+    "url": "/home/static/css/main.20671097.chunk.css"
+  },
+  {
+    "revision": "23142f3ff957493a9a9d",
+    "url": "/home/static/js/2.e8be2d8b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/2.e8be2d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea414c97ea58d3bc14cd",
+    "url": "/home/static/js/main.a476b2a8.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "2b6e92b7bc93998bdadda35ea09d360a",
+    "url": "/home/static/media/akhil-dp.2b6e92b7.jpeg"
+  },
+  {
+    "revision": "353105a86e1a8b97b2c2378c55722f15",
+    "url": "/home/static/media/resume.353105a8.pdf"
+  }
+]);
